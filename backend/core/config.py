870726f1d67d25cb75d5775124d3ca4194ce9506
Peter Kačmarík <124926474+peterkacmarik@users.noreply.github.com>
2024-10-23 22:03:53 +0200

    Update config.py

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,30 +1,30 @@
- # Načítanie konfiguračných údajov pomocou pydantic Settings
+#  # Načítanie konfiguračných údajov pomocou pydantic Settings
  
-from pydantic_settings import BaseSettings, SettingsConfigDict
+# from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
-    MAIL_USERNAME: str
-    MAIL_PASSWORD: str
-    MAIL_SERVER: str
-    MAIL_PORT: int
-    MAIL_FROM: str
-    MAIL_FROM_NAME: str
-    MAIL_STARTTLS: bool = True
-    MAIL_SSL_TLS: bool = False
-    USE_CREDENTIALS: bool = True
-    VALIDATE_CERTS: bool = True
-    JWT_ALGORITHM: str
-    JWT_SALT: str
-    JWT_SECRET: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    SECRET_KEY: str
-    DOMAIN: str
-    SQLALCHEMY_DATABASE_URL: str = 'sqlite+aiosqlite:///./backend/database/db.db'
-    BASE_URL: str
+# class Settings(BaseSettings):
+#     MAIL_USERNAME: str
+#     MAIL_PASSWORD: str
+#     MAIL_SERVER: str
+#     MAIL_PORT: int
+#     MAIL_FROM: str
+#     MAIL_FROM_NAME: str
+#     MAIL_STARTTLS: bool = True
+#     MAIL_SSL_TLS: bool = False
+#     USE_CREDENTIALS: bool = True
+#     VALIDATE_CERTS: bool = True
+#     JWT_ALGORITHM: str
+#     JWT_SALT: str
+#     JWT_SECRET: str
+#     ACCESS_TOKEN_EXPIRE_MINUTES: int
+#     SECRET_KEY: str
+#     DOMAIN: str
+#     SQLALCHEMY_DATABASE_URL: str = 'sqlite+aiosqlite:///./backend/database/db.db'
+#     BASE_URL: str
     
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+#     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
         
-Config = Settings()
-# print(Settings().model_dump())
+# Config = Settings()
+# # print(Settings().model_dump())
